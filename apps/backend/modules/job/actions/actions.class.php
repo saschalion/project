95cafaceaec0a279d927cdd5fa4861fc3a54ad54
Alex <saschalion@gmail.com>
2011-04-06 14:54:00 +0700

    JS for form has been edited

--- apps/backend/modules/job/actions/actions.class.php
+++ apps/backend/modules/job/actions/actions.class.php
@@ -17,7 +17,7 @@ class jobActions extends autoJobActions
         {
             parent::preExecute();
             $this->getResponse()->addJavascript('/js/form.js', 'last');
-            $this->getResponse()->addJavascript('/js/jquery-1.4.4.min.js', 'last');
+            $this->getResponse()->addJavascript('/js/jquery-1.4.4.min.js', 'first');
         }
 
 
--- web/js/form.js
+++ web/js/form.js
@@ -1,15 +1,46 @@
-$(document).ready(function() {
-    hideShowPassport();
+$(function() {
+    hidePassport();
+    showPassport();
     hideShowLanguage();
     hideShowEducational();
     hideShowEducationalExtra();
     hideShowProfession();
     hideShowMilitary();
     hideShowLeaving();
+
+    $('#show-passport').click(function(){
+        var obj = $('#sf_fieldset_______________');
+        if( obj.css('display')=='none' )
+            obj.slideDown(400);
+        else
+            obj.slideUp(400);
+    });
+    
+//    $('#jobeet_job_type_M').change(function(){
+//        var obj = $('#sf_fieldset_______________');
+//        if( $(this).attr('checked')=='checked' )
+//            obj.slideUp(400);
+//    });
+
+  
+      $('#jobeet_job_type_M').change(function(){
+        $('#sf_fieldset__________________________').slideDown(400);
+    });
+
+    $('#jobeet_job_type___').change(function(){
+        $('#sf_fieldset__________________________').slideUp(400);
+    });
+
+
 });
 
-function hideShowPassport(){ 
-    $('#sf_fieldset_______________').toggle();
+function showPassport(){
+    
+    $('#sf_fieldset_______________').show();
+}
+
+function hidePassport(){
+    $('#sf_fieldset_______________').hide();
 }
 
 function hideShowLanguage(){ 
@@ -35,7 +66,3 @@ function hideShowMilitary(){
 function hideShowLeaving(){
     $('#sf_fieldset_____________').toggle();
 }
-
-
-
-
